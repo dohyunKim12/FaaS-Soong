15155adb1c2a4df250a4df0a08fd5be5e470c887
dohyunkim <kdh951102@gmail.com>
2021-09-08 08:14:39 +0000

    20210908

--- rabbitMQ/send.py
+++ rabbitMQ/send.py
@@ -6,8 +6,8 @@ connection = pika.BlockingConnection(pika.URLParameters('amqp://faasoong:tnd@116
     #pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='accident')
 
-channel.basic_publish(exchange='', routing_key='hello', body='100')
-print(" [x] Sent 'Hello World!'")
+channel.basic_publish(exchange='', routing_key='accident', body='100')
+print(" [x] Sent 'Accidnet occur!'")
 connection.close()
